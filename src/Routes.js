d3ee9b1c4d57aa789efbecd4d19936196234e468
Yash Rajgor <yash592@gmail.com>
2019-05-15 15:33:46 -0700

    MAterial-UI

--- src/Routes.js
+++ src/Routes.js
@@ -2,16 +2,17 @@ import React from "react";
 import { Scene, Router, Actions } from "react-native-router-flux";
 import LoginForm from "./components/LoginForm/LoginForm";
 import Home from "./components/Home/Home";
+import NewsDetail from "./components/NewsDetail/NewsDetail";
 
 const Routes = () => {
   return (
     <Router>
       <Scene key="root">
-        <Scene key="auth" hideNavBar>
+        <Scene key="auth" hideNavBar initial>
           <Scene key="login" component={LoginForm} />
         </Scene>
         <Scene key="main" hideNavBar initial>
-          <Scene key="Home" component={Home} />
+          <Scene key="NewsDetail" component={NewsDetail} />
         </Scene>
       </Scene>
     </Router>
