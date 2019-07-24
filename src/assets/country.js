f753cff87c9be94b1e913544f5e3b1b880742bb2
Yash Rajgor <yash592@gmail.com>
2019-07-23 23:46:55 -0700

    Country flags complete

---
+++ src/assets/country.js
@@ -0,0 +1,10 @@
+const country = [
+  {
+    id: 1,
+    name: "United Arab Emirates",
+    ISOcode: "ae",
+    flagImg: 'require("../../../assets/countryImages/united-arab-emirates.png")'
+  }
+];
+
+export { country };
