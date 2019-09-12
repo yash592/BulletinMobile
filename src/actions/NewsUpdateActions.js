2e3d9328d8345ce3a24a2deddde185cbe188a06c
Yash Rajgor <yash592@gmail.com>
2019-09-11 23:11:50 -0700

    News save skel

---
+++ src/actions/NewsUpdateActions.js
@@ -0,0 +1,7 @@
+import Axios from "axios";
+import { Actions } from "react-native-router-flux";
+import { NEWS } from "./types";
+
+export const saveStory = () => {};
+
+export const deleteStory = () => {};
