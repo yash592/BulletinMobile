765377ddde75c59decdb2c5f7304f56f1e4ee38b
Yash Rajgor <yash592@gmail.com>
2019-10-14 23:10:30 -0700

    summarizeAPIV1

--- src/actions/SummarizeActions.js
+++ src/actions/SummarizeActions.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import { SUMMARIZE_NEWS } from "./types";
 import { Actions } from "react-native-router-flux";
 // const unirest = require("unirest");
+const AYLIENTextAPI = require("aylien_textapi");
 
 // summarize: function(link) {
 //     console.log(link);
@@ -9,35 +10,29 @@ import { Actions } from "react-native-router-flux";
 //    .header("X-RapidAPI-Key", "ODvwbEGCc8mshFxn58WHl2tFdqkfp1eFXRXjsnlfTlgUdF0qML")
 //
 //   },
+var textapi = new AYLIENTextAPI({
+  application_id: "93bf2c79",
+  application_key: "2483722242baec84a6ab44965d4c96a7"
+});
 
 export const summarizeArticle = (link, img, title, author) => {
   // console.log("got to summarize", link, img, title, author);
-  let url = `https://aylien-text.p.rapidapi.com/summarize?url=${link}&sentences_number=5`;
+  let url = `https://api.aylien.com/api/v1/summarize`;
   // console.log(url);
   // console.log(fetch);
 
   return dispatch => {
-    return fetch(url, {
-      method: "GET",
-      headers: {
-        "X-RapidAPI-Key": "ODvwbEGCc8mshFxn58WHl2tFdqkfp1eFXRXjsnlfTlgUdF0qML"
+    textapi.summarize(
+      {
+        url: link
+      },
+      function(err, res) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("RES", res);
+        }
       }
-    }).then(res => {
-      Actions.newsdetail();
-      const response = JSON.parse(res._bodyText);
-      // console.log("API response", response);
-      dispatch({
-        type: SUMMARIZE_NEWS,
-        payload: [
-          {
-            summary: response.sentences,
-            link: link,
-            img: img,
-            title: title,
-            author: author
-          }
-        ]
-      });
-    });
+    );
   };
 };
