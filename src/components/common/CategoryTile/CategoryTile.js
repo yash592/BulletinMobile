67494becc06627da71afd4c68260e8d9882f90dc
Yash Rajgor <yash592@gmail.com>
2019-10-30 23:20:30 -0700

    stylef components primer

--- src/components/common/CategoryTile/CategoryTile.js
+++ src/components/common/CategoryTile/CategoryTile.js
@@ -9,6 +9,7 @@ import {
   Dimensions,
   ScrollView
 } from "react-native";
+import styled from "styled-components";
 
 // const categories = require("../../assets/categories");
 
@@ -16,63 +17,60 @@ const CategoryTile = props => {
   const { img, text, onPress, children } = props;
 
   return (
-    <TouchableOpacity style={styles.container} onPress={onPress}>
-      <ImageBackground
-        source={{ uri: img }}
-        style={styles.imgBox}
-        imageStyle={styles.imgStyle}
-      >
+    <Container onPress={onPress}>
+      <TileImage source={{ uri: img }} imageStyle={styles.imgStyle}>
         {children}
-        <View style={styles.textBox}>
-          <Text style={styles.text}>{text}</Text>
-        </View>
-      </ImageBackground>
-    </TouchableOpacity>
+        <TextBox>
+          <Title>{text}</Title>
+        </TextBox>
+      </TileImage>
+    </Container>
   );
 };
 
+const Container = styled.TouchableOpacity`
+  margin: 5px;
+  width: 47.5%;
+  height: 23.5%;
+
+  elevation: 5;
+  background-color: white;
+  border-radius: 10;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TileImage = styled.ImageBackground`
+  width: 100%;
+`;
+
+const TextBox = styled.View`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  padding: 15px;
+  justify-content: flex-start;
+`;
+
+const Title = styled.Text`
+fontSize: 22;
+fontFamily:Roboto;
+fontWeight: bold;
+shadow-color: white;
+textShadowOffset: { width: 0.1, height: 0.1 };
+shadowOpacity: 0.5;
+textShadowRadius: 1;
+`;
+
 const styles = {
-  container: {
-    margin: 5,
-    width: "47.5%",
-    height: "23.5%",
-    shadowColor: "black",
-    shadowOffset: { width: 20, height: 20 },
-    shadowOpacity: 1,
-    shadowRadius: 2,
-    elevation: 5,
-    backgroundColor: "white",
-    borderRadius: 10,
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  imgBox: {
-    width: "100%"
-  },
   imgStyle: {
     width: "100%",
     height: "100%",
     opacity: 0.2,
     position: "relative",
     borderRadius: 10
-  },
-  textBox: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    padding: 15,
-    justifyContent: "flex-start"
-  },
-  text: {
-    fontSize: 22,
-    fontFamily: "Roboto",
-    fontWeight: "bold",
-    shadowColor: "white",
-    textShadowOffset: { width: 0.1, height: 0.1 },
-    shadowOpacity: 0.5,
-    textShadowRadius: 1
   }
 };
 
