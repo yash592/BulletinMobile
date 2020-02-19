31e91cede8cf1ebdfe7036f7e899c0de79e4030b
Yash Rajgor <yashrajgor@Yashs-MBP.attlocal.net>
2020-02-18 23:57:53 -0800

    trying to load fonts and images thru redux

--- src/components/LoadImagesAndFonts/LoadImagesAndFonts.js
+++ src/components/LoadImagesAndFonts/LoadImagesAndFonts.js
@@ -1,16 +1,15 @@
-const cacheImages = () => {
-  return images.map(image => {
-    console.log(image);
-    if (typeof image === "string") {
-      return Image.prefetch(image);
-    } else {
-      return Asset.fromModule(image).downloadAsync();
-    }
-  });
-};
+// const cacheImages = () => {
+//   return images.map(image => {
+//     console.log(image);
+//     if (typeof image === "string") {
+//       return Image.prefetch(image);
+//     } else {
+//       return Asset.fromModule(image).downloadAsync();
+//     }
+//   });
+// };
 
 export const loadImages = async () => {
-  const imageAssets = cacheImages([
-    //require("../../assets/images/business.png")
-  ]);
+  console.log("Loaded Images fn");
+  const imageAssets = cacheImages([require("../assets/images/booking.png")]);
 };
